4f675254937ee43dbf90b9ac8c5ee8afe7ea9525
Anoop Simon <anoop.simon@adudev.com>
2019-03-27 11:29:13 +1100

    Added enumerable extensions

---
+++ Framework.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Shuffle the items in an enumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+    }
+}
