89569f0a875179516b7aace47a0d40fa35a96caa
Anoop <simon.anoopsimon.anoop@gmail.com>
2019-07-28 19:29:17 +1000

    Added JSON Schema validator

---
+++ Framework.Core/Utils/JSONUtil.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Core.Utils
+{
+    public class JSONUtil
+    {
+        public void CreateJSONSchema()
+        {
+            JSchema schema = JSchema.Parse(@"{
+              'type': 'object',
+              'properties': {
+                'name': {'type':'string'},
+                'roles': {'type': 'array'}
+              }
+            }");
+
+            JObject user = JObject.Parse(@"{
+  'name': 'Arnie Admin',
+  'roles': ['Developer', 'Administrator']
+}");
+
+            bool valid = user.IsValid(schema);
+        }
+    }
+}
